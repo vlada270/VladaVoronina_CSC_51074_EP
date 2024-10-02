7cce29541c195629ceee6f7bf51e407a0cfc5ac5
voronina_vlada <55885082+vlada270@users.noreply.github.com>
2024-10-02 23:35:48 +0200

    Delete TD1 directory

--- TD1/cmake-build-debug/deps/googletest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/vladavoronina/Desktop/de/code-skeleton-csc_51074_ep/deps/googletest/googlemock
-# Build directory: /Users/vladavoronina/Desktop/de/code-skeleton-csc_51074_ep/TD1/cmake-build-debug/deps/googletest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("../googletest")
--- TD1/cmake-build-debug/deps/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/vladavoronina/Desktop/de/code-skeleton-csc_51074_ep/deps/googletest/googletest
-# Build directory: /Users/vladavoronina/Desktop/de/code-skeleton-csc_51074_ep/TD1/cmake-build-debug/deps/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
