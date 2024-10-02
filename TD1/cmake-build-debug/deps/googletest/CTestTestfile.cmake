fc8617329cfe8861bfac2e7091c277b2231e5668
vladavoronina <vlada_voronina_00@bk.ru>
2024-10-02 22:50:41 +0200

    Submission TD 01

---
+++ TD1/cmake-build-debug/deps/googletest/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/vladavoronina/Desktop/de/code-skeleton-csc_51074_ep/deps/googletest
+# Build directory: /Users/vladavoronina/Desktop/de/code-skeleton-csc_51074_ep/TD1/cmake-build-debug/deps/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googlemock")
